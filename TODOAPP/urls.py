1b990389543e87348f1ee9aa18cb387bf08ee503
abas-dev <abasisaac22@gmail.com>
2023-02-11 03:45:57 +0100

    add urls to app

--- TODOAPP/urls.py
+++ TODOAPP/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from baseApp import urls as baseUrls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(baseUrls, namespace= 'base'))
 ]
